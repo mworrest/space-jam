939821568a0c42fc410f3b46e638bd40b90ee8a0
Caleb Heidel <cd.heidel92@gmail.com>
2018-11-17 11:03:07 -0500

    Added js includes for navbar and footer load-ins

---
+++ js/includes.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('#navbar').load('includes/navbar.html');
+    $('#sidebar_issues').load('includes/sidebar_issues.html');
+    $('#footer').load('includes/footer.html');
+
+    $('#btn_issues').click(function() {
+        console.log('hi');
+    });
+
+});
