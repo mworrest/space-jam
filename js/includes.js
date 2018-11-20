a583526a2dbcabc00b9d424285d54ed7fd9e58d2
Claudia <claudiayee93@gmail.com>
2018-11-19 21:18:13 -0500

    Added  #looney in order to load buggs bio when user opens page instead of click function-- issue with this.

--- js/includes.js
+++ js/includes.js
@@ -1,16 +1,18 @@
 $(document).ready(function () {
     $('#navbar').load('includes/navbar.html');
-    $('#sidebar_issues').load('includes/sidebar_issues.html');
     $('#footer').load('includes/footer.html');
+    $('#looney').load('includes/characters/bugs.html');
     
-    $('#jordan-trigger').click(function(){
-        $('#test-content').load( 'includes/characters/jordan.html' );
+    
+    
+    $('#jordan-trigger').click(function (){
+        $('#test-content').load('includes/characters/jordan.html');
     });
-    $('#ewing-trigger').click(function(){
-        $('#test-content').load( 'includes/characters/ewing.html' );
+    $('#ewing-trigger').click(function (){
+        $('#test-content').load('includes/characters/ewing.html');
     });
-    $('#bugs-trigger').click(function(){
-        $('#test-content').load( 'includes/characters/bugs.html' );
+    $('#bugs-trigger').click(function (){
+        $('#test-content').load('includes/characters/bugs.html');
     });
     
 
