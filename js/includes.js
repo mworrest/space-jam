1fd72fdea42526818cba88777fcbfaf5693f592e
Claudia <claudiayee93@gmail.com>
2018-11-21 21:22:39 -0500

    created looney character include pages. carousel links do not work yet

--- js/includes.js
+++ js/includes.js
@@ -14,6 +14,22 @@ $(document).ready(function () {
     $('#bugs-trigger').click(function (){
         $('#test-content').load('includes/characters/bugs.html');
     });
+    $('#daffy-trigger').click(function (){
+        $('#test-content').load('includes/characters/daffy.html');
+    });
+     $('#porky-trigger').click(function (){
+        $('#test-content').load('includes/characters/porky.html');
+    });
+    $('#tweety_syl-trigger').click(function (){
+        $('#test-content').load('includes/characters/tweety.html');
+    });
+    $('#elmer-trigger').click(function (){
+        $('#test-content').load('includes/characters/elmer.html');
+    });
+    $('#coyote-trigger').click(function (){
+        $('#test-content').load('includes/characters/coyote.html');
+    });
+    
     
 
 });
